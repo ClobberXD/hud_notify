3e34a4b7ce640f8e739979c472be47a3a352fae0
Anand S <36130650+ClobberXD@users.noreply.github.com>
2018-05-01 15:03:36 +0530

    Re-implemented using HUD elements instead of formspec

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 --
 -- /notify <player> <message>
 --
--- Sends a PM to an online player via a formspec (with the message) and an 'OK' button
+-- Sends a PM to an online player via HUD elements
 -- Restricted to only moderators, due to the potential for abuse
 --
+
 minetest.register_chatcommand("notify", {
 	params = "<player_name> <message>",
-	description = "Shows a message to player, by means of a formspec. e.g. /notify rubenwardy Stop spamming!",
+	description = "Shows a message to player using HUD elements. e.g. /notify LegendaryGriefer Stop griefing or face a ban!",
 	privs = {kick = true, ban = true},
 	func = function(name, param)
 		param = param:trim()
@@ -19,10 +20,37 @@ minetest.register_chatcommand("notify", {
 		end
 		local msg_header = "Notification from " ..name.. ": "
 		minetest.chat_send_player(name, "Notification sent to " ..player_name.. ": \"" ..msg.. "\"")
-		minetest.show_formspec(player_name, "notify:msg",
-			"size[8,4]" ..
-			"label[0.5,0;" ..msg_header.. "]" ..
-			"label[0.5,1;" ..msg.. "]" ..
-			"button_exit[2,3;4,2;exit;OK]")
+		
+		local player = minetest.get_player_by_name(player_name)
+		local hud_bg = player:hud_add({
+			hud_elem_type = "image",
+			position = {x = 0, y = 0},
+			offset = {x = 50, y = 300},
+			scale = {x = -30, y = -30},
+			alignment = {x = 1, y = 0},
+			text = "notify_bg.png"
+		})
+		local hud_header = player:hud_add({
+			hud_elem_type = "text",
+			position = {x = 0, y = 0},
+			offset = {x = 70, y = 200},
+			alignment = {x = 1, y = 0},
+			number = 0x3399FF,
+			text = msg_header
+		})
+		local hud_msg = player:hud_add({
+			hud_elem_type = "text",
+			position = {x = 0, y = 0},
+			offset = {x = 70, y = 250},
+			alignment = {x = 1, y = 0},
+			number = 0xFFFFFF,
+			text = msg
+		})
+		
+		minetest.after(10, function()
+			player:hud_remove(hud_bg)
+			player:hud_remove(hud_header)
+			player:hud_remove(hud_msg)
+		end)
 	end
 })
