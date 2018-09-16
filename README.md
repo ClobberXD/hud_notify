8bbd0d26e42ae4d3def87692f3e8c244420a0dba
Anand S <ClobberXD@gmail.com>
2018-09-16 22:38:12 +0530

    Major fixes and improvements
    - Use local table to store HUD IDs
    - Use local notify function. Separate from chat-command def
    - Use separate update function with globalstep

--- README.md
+++ README.md
@@ -5,20 +5,25 @@
 HUD Notify is a very simple mod that comprises of just one command `/notify`, which is _very_ similar to the builtin `/msg` command, except for the fact that /notify displays a message using HUD elements (instead of displaying via chat) for better noticeability. Due to the higher potential for abuse, the use of this command is restricted to moderators and above.
 
 ### Usage
+
 `/notify <player_name> <msg>`
 
 ### Example
+
 `/notify LegendaryGriefer Stop griefing or face a ban!`
 
 ### minetest.conf
+
 - `notify.hud_duration`
 
 Stores a numeric value (10 by default) which represents the duration of the message visibility in seconds.
 
 ### Screenshot
+
 ![screenshot](https://raw.githubusercontent.com/ClobberXD/hud_notify/master/screenshot.png)
 
 ### TODO
-- [x] Re-Implement using HUD element to get rid of it's [disadvantages](https://forum.minetest.net/viewtopic.php?p=317428#p317428). (Fixed with [3e34a4b](https://github.com/ClobberXD/hud_notify/commit/3e34a4b7ce640f8e739979c472be47a3a352fae0))
+
+- [x] Re-Implement using HUD element to get rid of its [disadvantages](https://forum.minetest.net/viewtopic.php?p=317428#p317428). (Fixed with [3e34a4b](https://github.com/ClobberXD/hud_notify/commit/3e34a4b7ce640f8e739979c472be47a3a352fae0))
 - [ ] Add automatic line-break if `msg` too long.
 - [ ] Implement global notification functionality.
--- init.lua
+++ init.lua
@@ -5,53 +5,75 @@
 -- Restricted to only moderators, due to the potential for abuse
 --
 
-local setting = "hud_notify.duration"
+local players = {}
+local duration = tonumber(minetest.settings:get("hud_notify.duration")) or 10
+local hide_sender = minetest.settings:get_bool("hud_notify.hide_sender") or false
 
-minetest.register_chatcommand("notify", {
-	params = "<player_name> <message>",
-	description = "Shows a message to player using HUD elements. e.g. /notify LegendaryGriefer Stop griefing or face a ban!",
-	privs = {kick = true, ban = true},
-	func = function(name, param)
-		param = param:trim()
-		local player_name, msg = param:match("^(%S+)%s(.+)$")
-		if not player_name then
-			return false, "Invalid usage, see /help notify."
-		end
-		if not core.get_player_by_name(player_name) then
-			return false, "The player " ..player_name.. " is not online."
-		end
-		minetest.chat_send_player(name, "Notification sent to " ..player_name.. ": \"" ..msg.. "\"")
-		
-		local player = minetest.get_player_by_name(player_name)
-		local hud_bg = player:hud_add({
-			hud_elem_type = "image",
-			position = {x = 0, y = 0},
-			offset = {x = 50, y = 300},
-			scale = {x = -30, y = -30},
-			alignment = {x = 1, y = 0},
-			text = "hud_notify_bg.png"
-		})
-		local hud_msg = player:hud_add({
+local function notify(name, param)
+	local pname, msg = param:trim():match("^(%S+)%s(.+)$")
+	if not pname or not msg then
+		return false, "Invalid usage, see /help notify."
+	end
+	if not core.get_player_by_name(pname) then
+		return false, "The player " .. pname .. " is not online."
+	end
+
+	local player = minetest.get_player_by_name(pname)
+	players[pname] = {time = os.time() + duration}
+	players[pname].bg = player:hud_add({
+		hud_elem_type = "image",
+		position = {x = 0, y = 0},
+		offset = {x = 50, y = 300},
+		scale = {x = -30, y = -30},
+		alignment = {x = 1, y = 0},
+		text = "hud_notify_bg.png"
+	})
+	players[pname].msg = player:hud_add({
+		hud_elem_type = "text",
+		position = {x = 0, y = 0},
+		offset = {x = 70, y = 230},
+		alignment = {x = 1, y = 0},
+		number = 0xFFFFFF,
+		text = msg
+	})
+
+	if not hide_sender then
+		local msg_header = "Notification from " .. name .. ": "
+		players[pname].header = player:hud_add({
 			hud_elem_type = "text",
 			position = {x = 0, y = 0},
-			offset = {x = 70, y = 210},
+			offset = {x = 70, y = 200},
 			alignment = {x = 1, y = 0},
-			number = 0xFFFFFF,
-			text = msg
+			number = 0x3399FF,
+			text = msg_header,
+			text = "hud_notify_bg.png"
 		})
-		
-		local duration = tonumber(minetest.settings:get(setting))
-		if not duration then
-			duration = 10
-			minetest.settings:set(setting, "10")
-		end
-		
-		minetest.after(duration, function()
-			local player = minetest.get_player_by_name(player_name)
-			if player then
-				player:hud_remove(hud_bg)
-				player:hud_remove(hud_msg)
-			end
-		end)
 	end
+
+	return true, "Notification sent to " .. pname .. ": \"" .. msg .. "\""
+end
+
+local function update()
+	for name, hud in pairs(players) do
+		if hud and os.time() > hud.time then
+			local player = minetest.get_player_by_name(name)
+			player:hud_remove(hud.bg)
+			player:hud_remove(hud.msg)
+			if not hide_sender then
+				player:hud_remove(hud.header)
+			end
+
+			players[name] = nil
+		end
+	end
+end
+
+minetest.register_chatcommand("notify", {
+	params = "<pname> <message>",
+	description = "Shows a message to player using HUD elements. e.g. "
+					.. "/notify LegendaryGriefer Stop griefing or face a ban!",
+	privs = {kick = true, ban = true},
+	func = notify
 })
+
+minetest.register_globalstep(update)
