f16078326df11d9fdf1763dbccf400f8b377ee4a
Anand S <ClobberXD@gmail.com>
2018-09-17 12:47:17 +0530

    Add support for server-wide notifications

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 # HUD Notify [hud_notify]
 
-### Dependencies: None
+HUD Notify is a mod that can be used display messages using HUD elements (instead of chat) for better noticeability. Due to the higher potential for abuse, the use of this command is restricted to moderators and above.
 
-HUD Notify is a very simple mod that comprises of just one command `/notify`, which is _very_ similar to the builtin `/msg` command, except for the fact that /notify displays a message using HUD elements (instead of displaying via chat) for better noticeability. Due to the higher potential for abuse, the use of this command is restricted to moderators and above.
+### Dependencies: None
 
 ### Usage
 
-`/notify <player_name> <msg>`
+- `/notify <player_name> <msg>`
+  - Send a notification (`msg`) to `name`.
+- `/notify_all <msg>`
+  - Send a global notification `msg`.
 
 ### Example
 
 `/notify LegendaryGriefer Stop griefing or face a ban!`
+`/notify_all Attention! The server will be shutting down for maintenance in 15 minutes.`
 
 ### minetest.conf
 
-- `notify.hud_duration`
-
-Stores a numeric value (10 by default) which represents the duration of the message visibility in seconds.
+- `hud_notify.hud_duration`
+  - Stores a numeric value (10 by default) which represents the duration of the message visibility in seconds.
+- `hud_notify.hide_sender`
+  - Boolean value which is evaluated to show or hide message header containing the sender's name.
 
 ### Screenshot
 
@@ -24,6 +29,4 @@ Stores a numeric value (10 by default) which represents the duration of the mess
 
 ### TODO
 
-- [x] Re-Implement using HUD element to get rid of its [disadvantages](https://forum.minetest.net/viewtopic.php?p=317428#p317428). (Fixed with [3e34a4b](https://github.com/ClobberXD/hud_notify/commit/3e34a4b7ce640f8e739979c472be47a3a352fae0))
-- [ ] Add automatic line-break if `msg` too long.
-- [ ] Implement global notification functionality.
+- Add automatic line-break if `msg` too long.
--- init.lua
+++ init.lua
@@ -1,26 +1,33 @@
 --
 -- /notify <player> <message>
+-- /notify_all <message>
 --
--- Sends a PM to an online player using HUD elements
--- Restricted to only moderators, due to the potential for abuse
+-- Sends a message to online player(s) using HUD elements.
+-- Restricted to moderators, due to the potential for abuse.
 --
 
 local players = {}
 local duration = tonumber(minetest.settings:get("hud_notify.duration")) or 10
 local hide_sender = minetest.settings:get_bool("hud_notify.hide_sender") or false
 
-local function notify(name, param)
-	local pname, msg = param:trim():match("^(%S+)%s(.+)$")
-	if not pname or not msg then
-		return false, "Invalid usage, see /help notify."
-	end
-	if not core.get_player_by_name(pname) then
-		return false, "The player " .. pname .. " is not online."
+local function notify(name, msg)
+	local player = minetest.get_player_by_name(name)
+
+	-- If notification is already being shown modify
+	-- msg (and header if it exists) and time
+	if players[name] then
+		players[name].time = os.time() + duration
+		player:hud_change(players[name].msg, "text", msg)
+		if not hide_sender then
+			player:hud_change(players[name].header, "text",
+								"Notification from " .. name .. ": ")
+		end
+		return
 	end
 
-	local player = minetest.get_player_by_name(pname)
-	players[pname] = {time = os.time() + duration}
-	players[pname].bg = player:hud_add({
+	-- Else, add new elements
+	players[name] = {time = os.time() + duration}
+	players[name].bg = player:hud_add({
 		hud_elem_type = "image",
 		position = {x = 0, y = 0},
 		offset = {x = 50, y = 300},
@@ -28,7 +35,7 @@ local function notify(name, param)
 		alignment = {x = 1, y = 0},
 		text = "hud_notify_bg.png"
 	})
-	players[pname].msg = player:hud_add({
+	players[name].msg = player:hud_add({
 		hud_elem_type = "text",
 		position = {x = 0, y = 0},
 		offset = {x = 70, y = 230},
@@ -39,7 +46,7 @@ local function notify(name, param)
 
 	if not hide_sender then
 		local msg_header = "Notification from " .. name .. ": "
-		players[pname].header = player:hud_add({
+		players[name].header = player:hud_add({
 			hud_elem_type = "text",
 			position = {x = 0, y = 0},
 			offset = {x = 70, y = 200},
@@ -49,8 +56,6 @@ local function notify(name, param)
 			text = "hud_notify_bg.png"
 		})
 	end
-
-	return true, "Notification sent to " .. pname .. ": \"" .. msg .. "\""
 end
 
 local function update()
@@ -70,10 +75,37 @@ end
 
 minetest.register_chatcommand("notify", {
 	params = "<pname> <message>",
-	description = "Shows a message to player using HUD elements. e.g. "
-					.. "/notify LegendaryGriefer Stop griefing or face a ban!",
+	description = "Shows a message to player using HUD elements.",
 	privs = {kick = true, ban = true},
-	func = notify
+	func = function(name, param)
+		local pname, msg = param:trim():match("^(%S+)%s(.+)$")
+		if not pname or not msg then
+			return false, "Invalid usage, see /help notify."
+		end
+		if not core.get_player_by_name(pname) then
+			return false, "The player " .. pname .. " is not online."
+		end
+
+		notify(pname, msg)
+		return true, "Notification sent to " .. pname .. ": \"" .. msg .. "\""
+	end
+})
+
+minetest.register_chatcommand("notify_all", {
+	params = "<message>",
+	description = "Shows a message to all online players using HUD elements.",
+	privs = {kick = true, ban = true},
+	func = function(name, param)
+		local msg = param:trim()
+		if not msg then
+			return false, "Invalid usage, see /help notify_all."
+		end
+
+		for _, player in pairs(minetest.get_connected_players()) do
+			notify(player:get_player_name(), msg)
+		end
+		return true, "Global notification sent: \"" .. msg .. "\""
+	end
 })
 
 minetest.register_globalstep(update)
